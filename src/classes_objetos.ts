8bf79f6a21265ec5944cb329980ce96f0676e64e
RenanCruz7 <renancruz199@gmail.com>
2025-10-08 18:27:01 -0300

    Feat: Classes e objetos

---
+++ src/classes_objetos.ts
@@ -0,0 +1,12 @@
+class User {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+const user = new User('Renan', 30);
+console.log(user);
